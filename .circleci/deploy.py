9c71665c54486ef4f9cfcbeca8e81bee7f9002b6
roquie <roquie0@gmail.com>
2019-01-13 01:17:13 +0300

    Test deploy with flynn.

--- .circleci/deploy.py
+++
@@ -1,8 +0,0 @@
-import heroku3
-import os
-
-heroku = heroku3.from_key(os.environ['HEROKU_API_KEY'])
-
-# app = heroku_conn.apps()['sharp-night-7758']
-
-app = heroku_conn.create_app(name=os.environ['MOCK_APP_DOMAIN'], region_id_or_name='eu')
